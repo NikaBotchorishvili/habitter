7af35444c0e8cf6946c67b9a07ca023ae18c7340
Nika Botchorishvili <nikabotchorishvili@gmail.com>
2024-07-10 21:15:49 +0400

    fix: tackled production lint error and possibly fixed the middleware

--- src/components/Journal/Form/AddForm.tsx
+++ src/components/Journal/Form/AddForm.tsx
@@ -24,31 +24,32 @@ const JournalAddForm = () => {
 	return (
 		<AnimatePresence>
 			{addToggled && (
-				<Overlay onClick={() => setAddToggled(false)}>
-					<motion.section
-						className="absolute left-1/2 -translate-x-1/2 md:max-w-[400px] w-full"
-						initial={{ top: -1000 }}
-						animate={{ top: 150 }}
-						exit={{ top: -1000 }}
-					>
-						<div
-							onClick={(e) => {
-								e.stopPropagation();
-							}}
-							className="flex flex-col gap-y-3 w-full bg-lightModeSecondary dark:bg-darkModeSecondary p-5 rounded-md z-20"
+				<>
+					<Overlay onClick={() => {setAddToggled(false)}}/>
+						<motion.section
+							className="absolute left-1/2 -translate-x-1/2 md:max-w-[400px] w-full"
+							initial={{ top: -1000 }}
+							animate={{ top: 150 }}
+							exit={{ top: -1000 }}
 						>
-							<FontAwesomeIcon
-								onClick={() => setAddToggled(false)}
-								className="cursor-pointer left-5  -top-8 absolute size-[25px]"
-								icon={faX}
-							/>
-							<h2 className="text-center text-xl">
-								New journal entry
-							</h2>
-							<Form onSubmit={onSubmit}/>
-						</div>
-					</motion.section>
-				</Overlay>
+							<div
+								onClick={(e) => {
+									e.stopPropagation();
+								}}
+								className="flex flex-col gap-y-3 w-full bg-lightModeSecondary dark:bg-darkModeSecondary p-5 rounded-md z-20"
+							>
+								<FontAwesomeIcon
+									onClick={() => setAddToggled(false)}
+									className="cursor-pointer left-5  -top-8 absolute size-[25px]"
+									icon={faX}
+								/>
+								<h2 className="text-center text-xl">
+									New journal entry
+								</h2>
+								<Form onSubmit={onSubmit}/>
+							</div>
+						</motion.section>
+				</>
 			)}
 			<FontAwesomeIcon
 				key="addButton"
--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,43 +1,45 @@
-import { createServerClient } from '@supabase/ssr';
-import { NextRequest, NextResponse } from 'next/server';
+import { createServerClient } from "@supabase/ssr";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function updateSession(request: NextRequest) {
-  let supabaseResponse = NextResponse.next();
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll: () => request.cookies.getAll(),
-        setAll: (cookiesToSet) => {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            request.cookies.set(name, value)
-          );
-          supabaseResponse = NextResponse.next();
-          cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
-          );
-        }
-      }
-    }
-  );
+	let supabaseResponse = NextResponse.next({request: request});
+	const supabase = createServerClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		{
+			cookies: {
+				getAll: () => request.cookies.getAll(),
+				setAll: (cookiesToSet) => {
+					cookiesToSet.forEach(({ name, value, options }) =>
+						request.cookies.set(name, value)
+					);
+					supabaseResponse = NextResponse.next({request: request});
+					cookiesToSet.forEach(({ name, value, options }) =>
+						supabaseResponse.cookies.set(name, value, options)
+					);
+				},
+			},
+		}
+	);
 
-  const { data: { user } } = await supabase.auth.getUser();
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
 
-  if (request.nextUrl.pathname.startsWith('/api')) {
-    return supabaseResponse;
-  }
+	if (request.nextUrl.pathname.startsWith("/api")) {
+		return supabaseResponse;
+	}
 
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth') &&
-    !request.nextUrl.pathname.startsWith('/register')
-  ) {
-    const url = request.nextUrl.clone();
-    url.pathname = '/login';
-    return NextResponse.redirect(url);
-  }
+	if (
+		!user &&
+		!request.nextUrl.pathname.startsWith("/login") &&
+		!request.nextUrl.pathname.startsWith("/auth") &&
+		!request.nextUrl.pathname.startsWith("/register")
+	) {
+		const url = request.nextUrl.clone();
+		url.pathname = "/login";
+		return NextResponse.redirect(url);
+	}
 
-  return supabaseResponse;
+	return supabaseResponse;
 }
